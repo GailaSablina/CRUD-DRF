5526f0ae8b8dfb1d71bb8824de4165579306103f
GalinaSablina <125545793+GailaSablina@users.noreply.github.com>
2023-12-02 02:45:01 +0300

    Update views.py

--- stocks_products/logistic/views.py
+++ stocks_products/logistic/views.py
@@ -20,3 +20,14 @@ class StockViewSet(ModelViewSet):
     filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_class = StockFilter
     search_fields = ['positions__product__title', 'positions__product__description']
+    
+class CustomPagination(pagination.PageNumberPagination):    
+   def get_paginated_response(self, data):
+        return Response({
+            'links': {
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link()
+            },
+            'count': self.page.paginator.count,
+            'results': data
+        })  
